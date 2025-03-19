76fa348b80868c572cf6d70b2b5207766801e1f1
PrashantBTech <dhimanprashant996@gmail.com>
2025-03-19 21:48:07 +0530

    Create fiftyeigth58.java

---
+++ fiftyeigth58.java
@@ -0,0 +1,32 @@
+// Program of abstract class
+
+public class fiftyeigth58 {
+public static void main(String[] args) {
+    mustang m= new mustang();
+    //Animal -> horse -> mustang
+    m.walk();
+    horse h = new horse();
+    h.walk();
+    
+}
+}
+abstract class Animal{
+Animal(){
+    System.out.println("Animal Constructor called");
+}
+abstract void walk();
+}
+
+class horse extends Animal{
+    horse(){
+        System.out.println("horse Constructor called");
+    }
+    void walk(){
+        System.out.println("Walks on 4 legs");
+    }
+}
+class mustang extends horse{
+    mustang(){
+        System.out.println("mustang Constructor called");
+    }
+}
