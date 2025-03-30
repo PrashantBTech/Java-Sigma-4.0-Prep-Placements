408c7f7e13cf840f4b44bfca58cb5305e612f77c
PrashantBTech <dhimanprashant996@gmail.com>
2025-03-30 18:58:16 +0530

    Create seventyone71.java

---
+++ seventyone71.java
@@ -0,0 +1,58 @@
+// Merge Sort Program
+public class seventyone71 {
+
+    public static void printArr(int arr[]){
+        for(int i = 0; i < arr.length; i++){
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+
+    public static void mergeSort(int arr[], int si, int ei) {
+        if (si >= ei) {
+            return;
+        }
+        int mid = si + (ei - si) / 2; // or (si + ei)/2
+        mergeSort(arr, si, mid);
+        mergeSort(arr, mid + 1, ei);
+
+        merge(arr, si, mid, ei);
+    }
+
+    public static void merge(int arr[], int si, int mid, int ei) {
+        int temp[] = new int[ei - si + 1];
+        int i = si; // Index for first sorted part
+        int j = mid + 1; // Index for second sorted part
+        int k = 0; // Index for temp array
+
+        // Merge both sorted halves into temp array
+        while (i <= mid && j <= ei) {
+            if (arr[i] < arr[j]) {
+                temp[k++] = arr[i++];
+            } else {
+                temp[k++] = arr[j++];
+            }
+        }
+
+        // Copy any remaining elements from the first half
+        while (i <= mid) {
+            temp[k++] = arr[i++];
+        }
+
+        // Copy any remaining elements from the second half
+        while (j <= ei) {
+            temp[k++] = arr[j++];
+        }
+
+        // Copy temp array back to original array
+        for (k = 0, i = si; k < temp.length; k++, i++) {
+            arr[i] = temp[k];
+        }
+    }
+
+    public static void main(String[] args) {
+        int arr[] = { 6, 3, 9, 5, 2, 8 };
+        mergeSort(arr, 0, arr.length - 1);
+        printArr(arr);
+    }
+}
