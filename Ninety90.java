92d273029447e89123db2be024634a6b9634f6b2
PrashantBTech <dhimanprashant996@gmail.com>
2025-04-15 21:46:01 +0530

    Update Ninety90.java

--- Ninety90.java
+++ Ninety90.java
@@ -1,23 +1,23 @@
 // Linked List
 // Adding a node in Linked List,  TC: O(1)
 
-public class Ninety90{
+public class Ninety90 {
 
-    public static class Node{
+    public static class Node {
         int data;
         Node next;
 
-        public Node(int data){
+        public Node(int data) {
             this.data = data;
             this.next = null;
-        
+
         }
     }
 
-    public static Node head;  // Head Pointer , points to first node
-    public static Node tail;  // Tail Pointer , points to last node
+    public static Node head; // Head Pointer , points to first node
+    public static Node tail; // Tail Pointer , points to last node
 
-    public void addFirst(int data){
+    public void addFirst(int data) {
         // Step1 => create new node
         Node newNode = new Node(data);
         if (head == null) {
@@ -32,13 +32,27 @@ public class Ninety90{
         head = newNode;
     }
 
+    public void addLast(int data) {
+        Node newNode = new Node(data);
+        if (head == null) {
+            head = tail = newNode;
+            return;
+        }
+        tail.next = newNode;
+        tail = newNode;
+    }
+
     public static void main(String[] args) {
         Ninety90 ll = new Ninety90();
         ll.addFirst(1);
         ll.addFirst(2);
+        /*
+         * head -> [2] -> [1] -> null
+         *                 ↑
+         *                tail
+         */
+        ll.addLast(3);
+        ll.addLast(4);
+
     }
 }
-/*  head -> [2] -> [1] -> null
-                     ↑
-                     tail
-*/               
