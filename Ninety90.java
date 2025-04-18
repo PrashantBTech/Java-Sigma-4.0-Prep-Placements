09eb8f9331ce1f0e686ea824d7b9659e3a8b1158
PrashantBTech <dhimanprashant996@gmail.com>
2025-04-18 21:41:50 +0530

    Update Ninety90.java

--- Ninety90.java
+++ Ninety90.java
@@ -1,5 +1,6 @@
 // Linked List
 // Adding a node in Linked List,  TC: O(1)
+// printing the linked list, TC : O(n)
 
 public class Ninety90 {
 
@@ -42,17 +43,30 @@ public class Ninety90 {
         tail = newNode;
     }
 
+    public void Printll() {
+        if (head == null) {
+            System.out.println("Empty");
+        }
+        Node temp = head;
+        while (temp != null) {
+            System.out.print(temp.data + "->");
+            temp = temp.next;
+        }
+        System.out.println("null");
+    }
+
     public static void main(String[] args) {
         Ninety90 ll = new Ninety90();
-        ll.addFirst(1);
         ll.addFirst(2);
+        ll.addFirst(1);
         /*
          * head -> [2] -> [1] -> null
-         *                 ↑
-         *                tail
+         * ↑
+         * tail
          */
         ll.addLast(3);
         ll.addLast(4);
+        ll.Printll();
 
     }
 }
