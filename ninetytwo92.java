41541f3d10fe5ae4af45efba795f688b86e88f0c
PrashantBTech <dhimanprashant996@gmail.com>
2025-04-23 21:33:27 +0530

    Update ninetytwo92.java

--- ninetytwo92.java
+++ ninetytwo92.java
@@ -1,6 +1,8 @@
 // Detect a loop / cycle in a Linked List
 // by floyds cycle finding algorithm
 
+// Remove the loop / cycle from the linked list
+
 public class ninetytwo92 {
 
     public static class Node {
@@ -32,13 +34,47 @@ public class ninetytwo92 {
         return false;
     }
 
+    public static void removeCycle(){
+        // detect cycle
+        Node slow = head;
+        Node fast = head;
+        boolean cycle = false;
+        while (fast != null && fast.next != null) {
+            slow = slow.next; // +1
+            fast = fast.next.next; // +2
+            if (slow == fast) { // cycle exists
+                cycle = true;
+                break;
+
+            }
+        }
+        if (cycle == false) {
+            return;
+        }
+        // find meeting point
+        slow = head;
+        Node prev = null; // last node
+        while (slow != fast) {
+            prev = fast;
+            slow = slow.next;
+            fast = fast.next;
+        }
+        // remove cycle -> last.next = null
+        prev.next = null;
+
+    }
+
     public static void main(String[] args) {
         head = new Node(1);
-        head.next = new Node(2);
+        Node temp = new Node(2);
+        head.next = temp;
         head.next.next = new Node(3);
-        head.next.next.next = head;
-        // 1->2->3->1
-        System.out.println(isCycle());
+        head.next.next.next = temp;
+        // 1->2->3->2
+        System.out.println("Cycle exists ? :"+isCycle());
+        
+        removeCycle();
+        System.out.println("After removing cycle : "+isCycle());
 
 
     }
