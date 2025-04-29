52bedd883e6ee4b406c53d6863c650e3bc5c2500
PrashantBTech <dhimanprashant996@gmail.com>
2025-04-29 21:46:12 +0530

    Create ninetyfive95.java

---
+++ ninetyfive95.java
@@ -0,0 +1,103 @@
+// Zig-Zag Linked List
+// 1->2->3->4->5 to
+// 1->5->2->4->3
+
+import java.util.LinkedList;
+
+public class ninetyfive95 {
+    public static class Node {
+        int data;
+        Node next;
+        public Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    public static Node head;
+    public static Node tail;
+
+    public void addFirst(int data) {
+        Node newNode = new Node(data);
+        if (head == null) {
+            head = tail = newNode;
+            return;
+        }
+        newNode.next = head;
+        head = newNode;
+    }
+
+    public void addLast(int data) {
+        Node newNode = new Node(data);
+        if (head == null) {
+            head = tail = newNode;
+            return;
+        }
+        tail.next = newNode;
+        tail = newNode;
+    }
+
+    public void printLL() {
+        if (head == null) {
+            System.out.println("Empty");
+            return;
+        }
+        Node temp = head;
+        while (temp != null) {
+            System.out.print(temp.data + " -> ");
+            temp = temp.next;
+        }
+        System.out.println("null");
+    }
+
+
+    public void zigzag(){
+        // find mid
+        Node slow = head;
+        Node fast = head.next;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        Node mid= slow;
+        // reverse 2nd half
+        Node curr=  mid.next;
+        mid.next = null;
+        Node prev = null;
+        Node next;
+        while (curr != null) {
+            next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+        Node left = head;
+        Node right = prev;
+        Node nextL, nextR;
+        // alt merge - zig-zag merge
+        while (left != null && right != null) {
+            nextL = left.next;
+            left.next = right;
+            nextR = right.next;
+            right.next = nextL;
+
+            left = nextL;
+            right = nextR;
+        }
+
+    }
+
+    public static void main(String[] args) {
+        ninetyfive95 ll = new ninetyfive95();
+        ll.addLast(1);
+        ll.addLast(2);
+        ll.addLast(3);
+        ll.addLast(4);
+        ll.addLast(5);
+
+        ll.printLL();
+        ll.zigzag();
+        ll.printLL();
+        
+    }
+}
