f8c6671469e904113b12eff1c5d9480be697f4a1
PrashantBTech <dhimanprashant996@gmail.com>
2025-07-28 19:01:18 +0530

    Update hundrednineteen.java

--- hundrednineteen.java
+++ hundrednineteen.java
@@ -19,7 +19,7 @@ public class hundrednineteen {
         maxAct = 1;
         ans.add(0);
         int lastEnd = end[0];
-        for (int i = 0; i < end.length; i++) {
+        for (int i = 1; i < end.length; i++) {
             if (start[i] >= lastEnd) {
                 // activity select
                 maxAct++;
