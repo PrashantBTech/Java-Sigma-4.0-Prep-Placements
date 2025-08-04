3afe1261e4ae32b14580e8e5fe7ae0cdff2b4e4a
PrashantBTech <dhimanprashant996@gmail.com>
2025-08-04 20:15:30 +0530

    Update hundredtwentyseven127.java

--- hundredtwentyseven127.java
+++ hundredtwentyseven127.java
@@ -1,4 +1,5 @@
-// Question 4 :Best Time to Buy and Sell StockGivenanarrayprices[]oflengthN,representingthepricesofthestocksondifferentdays,thetaskistofindthemaximumprofitpossibleforbuyingandsellingthestocksondifferentdaysusing transactions where at most one transaction is allowed.Note: Stock must be bought before being sold.
+// Question 4 :Best Time to Buy and Sell Stock
+// Given an arrayprices[] of length N,representing the prices of the stocks on different days,the task is to find themaximumprofitpossibleforbuyingandsellingthestocksondifferentdaysusing transactions where at most one transaction is allowed.Note: Stock must be bought before being sold.
 // Sample Input 1:prices[] = {7, 6, 4, 3, 1}Sample Output 1: 0Sample Input 2:prices[] = {7, 1, 5, 3, 6, 4]Sample Output 2: 5
 
 public class hundredtwentyseven127 {
