14f6730e0a0bb10cfab01d79cc076bbb061e9801
PrashantBTech <dhimanprashant996@gmail.com>
2025-08-06 22:03:57 +0530

    Update hundredtwentyeight128.java

--- hundredtwentyeight128.java
+++ hundredtwentyeight128.java
@@ -1,6 +1,7 @@
 // Topic : Binary Tree
 // Pre-Order
 // Node -> LST -> RST
+// also we will implement the PreOrder Traversal
 
 
 public class hundredtwentyeight128 {
@@ -28,12 +29,27 @@ public class hundredtwentyeight128 {
 
             return newNode;
         }
+
+        // Traversal using PreOrder
+
+        public static void preorderTrav(Node root){  // tc: O(n)
+            if (root == null) {
+                System.out.print(-1+", ");
+                return ;
+            }
+            System.out.print(root.data+", ");
+            preorderTrav(root.left);
+            preorderTrav(root.right);
+        }
     }
+
+    
 
     public static void main(String[] args) {
         int nodes[]={1,2,4,-1,-1,5,-1,-1,3,-1,6,-1,-1};
         BinaryTree tree = new BinaryTree();
        Node root = tree.buildTree(nodes);
-        System.out.println(root.data); // root node
+                // Traversal using PreOrder
+       tree.preorderTrav(root);
     }
 }
