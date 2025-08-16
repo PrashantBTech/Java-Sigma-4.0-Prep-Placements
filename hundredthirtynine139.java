730131398b7d598c032641e0c896fce3f63e94bc
PrashantBTech <dhimanprashant996@gmail.com>
2025-08-16 17:48:36 +0530

    Create hundredthirtynine139.java

---
+++ hundredthirtynine139.java
@@ -0,0 +1,70 @@
+public class hundredthirtynine139 {
+    static class Node {
+        int data;
+        Node left;
+        Node right;
+
+        Node(int data) {
+            this.data = data;
+            this.left = null;
+            this.right = null;
+        }
+    }
+
+    public static int transform(Node root) {
+
+        if (root == null) {
+            return 0;
+        }
+        int leftChild = transform(root.left);
+        int rightChild = transform(root.right);
+
+        int data = root.data;
+
+        int newLeft = root.left == null ? 0 : root.left.data;
+        int newRight = root.right == null ? 0 : root.right.data;
+        root.data = newLeft + leftChild + newRight + rightChild;
+        return data;
+    }
+
+    public static void preorder(Node root) {
+        if (root == null) {
+            return;
+        }
+        System.out.print(root.data + " ");
+        preorder(root.left);
+        preorder(root.right);
+    }
+
+    public static void main(String[] args) {
+        /*
+         * 1 level =1
+         * / \
+         * 2 3 level=2
+         * / \ /\
+         * 4 5 6 7 level =3
+         */
+
+        // Expected ans :
+
+        /*
+         * 27 level =1
+         * / \
+         * 9 13 level=2
+         * / \ /\
+         * 0 0 0 0 level =3
+         */
+
+        Node root = new Node(1);
+        root.left = new Node(2);
+        root.right = new Node(3);
+        root.left.left = new Node(4);
+        root.left.right = new Node(5);
+        root.right.left = new Node(6);
+        root.right.right = new Node(7);
+
+        transform(root);
+        preorder(root);
+    }
+
+}
