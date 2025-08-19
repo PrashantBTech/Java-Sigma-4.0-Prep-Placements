bc3c4d0b996840353e428bb04988b8a27e730dd5
PrashantBTech <dhimanprashant996@gmail.com>
2025-08-19 11:21:42 +0530

    Update hundredfourty140.java

--- hundredfourty140.java
+++ hundredfourty140.java
@@ -1,5 +1,8 @@
 //  #  Binary Search Tree
 // Build a BST
+// Search in BST
+
+// Delete a node in BST
 
 public class hundredfourty140 {
     static class Node {
@@ -53,9 +56,44 @@ public class hundredfourty140 {
         }
     }
 
+    // Delete a node in BST
+
+    public static Node delete(Node root, int val){
+        if (root.data < val) {
+            root.right = delete(root.right, val);
+        }
+        else if (root.data > val) {
+            root.left = delete(root.left, val);
+        }else{
+            // case1 : leaf node
+            if (root.left == null && root.right == null) {
+                return null;
+            }
+            // case 2: having single child
+            if (root.left == null) {
+                return root.right;
+            }else if (root.right == null) {
+                return root.left;
+            }
+            // case 3: having two child
+            Node IS = findInorderSucessor(root.right);
+            root.data = IS.data;
+            root.right = delete(root.right, IS.data);
+        }
+        return root;
+    }
+    public static Node findInorderSucessor(Node root){
+        while (root.left != null) {
+            root = root.left;
+        }
+        return root;
+    }
+
 
     public static void main(String[] args) {
-        int values[] = { 5, 1, 3, 4, 2, 7 };
+        // int values[] = { 5, 1, 3, 4, 2, 7 };
+        
+        int values[] = {8,5,3,1,4,6,10,11,14};
         Node root = null;
         for (int i = 0; i < values.length; i++) {
             root = insertBST(root, values[i]);
@@ -63,5 +101,9 @@ public class hundredfourty140 {
         inorder(root);
         System.out.println();
         System.out.println("Found 4 : "+Search(root, 4));
+
+        System.out.println( "Deleting a node 5: " );
+        root = delete(root, 5);
+        inorder(root);
     }
 }
