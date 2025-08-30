e01c24ddd93789623bcea6baa5258042afd7a1f6
PrashantBTech <dhimanprashant996@gmail.com>
2025-08-30 18:03:15 +0530

    Update hundredfity150.java

--- hundredfity150.java
+++ hundredfity150.java
@@ -1,6 +1,7 @@
 //  Heaps
 // 1. Insertion 
 // 2. peek - get min
+// 3. Remove - fix using heapify
 
 import java.util.ArrayList;
 
@@ -21,11 +22,67 @@ public class hundredfity150 {
                 int temp = arr.get(x);
                 arr.set(x, arr.get(par));
                 arr.set(par, temp);
+
+                x = par;
+                par = (x-1)/2;
             }
         }
 
         public int peek(){
             return arr.get(0);
+        }
+
+        public void heapify(int i){
+            int left = 2*i+1;
+            int right =2*i+2;
+            int minidx = i;
+
+            if (left < arr.size() && arr.get(minidx) > arr.get(left)) {
+                minidx = left;
+            }
+             if (right < arr.size() && arr.get(minidx) > arr.get(right)) {
+                minidx = right;
+            }
+            if (minidx != i) {
+                // swap
+                int temp = arr.get(i);
+                arr.set(i,arr.get(minidx));
+                arr.set(minidx, temp);
+
+                heapify(minidx);
+            }
+        }
+
+        public int remove(){
+            int data = arr.get(0);
+            // step 1: swap first and last
+            int temp = arr.get(0);
+            arr.set(0, arr.get(arr.size()-1));
+            arr.set(arr.size()-1, temp);
+
+            // step 2: delete last
+            arr.remove(arr.size()-1);
+
+            // step 3: heapify
+            heapify(0);
+            return data;
+        }
+        
+        public boolean isEmpty(){
+            return arr.size() == 0;
+        }
+    }
+
+    public static void main(String[] args) {
+        Heap pq = new Heap();
+        pq.add(3);
+        pq.add(4);
+        pq.add(1);
+        pq.add(5);
+        
+        while (!pq.isEmpty()) {
+            System.out.println(pq.peek());
+            pq.remove();
         }
     }
 }
