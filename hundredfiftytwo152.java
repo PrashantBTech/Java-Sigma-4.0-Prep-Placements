660d3cb3d83f604cb0da807e1045cd4b80a2908e
PrashantBTech <dhimanprashant996@gmail.com>
2025-09-01 14:08:36 +0530

    Create hundredfiftytwo152.java

---
+++ hundredfiftytwo152.java
@@ -0,0 +1,36 @@
+// Nearby Cars
+// we are given N points in a 2D plne which are locations of N cars.
+// if we are at the origin, print the nearest k cars.
+
+import java.util.*;
+
+public class hundredfiftytwo152 {
+    static class Point implements Comparable<Point> {
+        int x, y, distSq, idx;
+
+        public Point(int x, int y, int distSq, int idx) {
+            this.x = x;
+            this.y = y;
+            this.distSq = distSq;
+            this.idx = idx;
+        }
+
+        @Override
+        public int compareTo(Point p2) {
+            return this.distSq - p2.distSq;
+        }
+    }
+
+    public static void main(String[] args) {
+        int pts[][]={{3,3},{5,-1},{-2,4}};
+        int k=2;
+        PriorityQueue<Point> pq = new PriorityQueue<>();
+        for(int i=0;i<pts.length;i++){
+            int distSq = pts[i][0]*pts[i][0] +  pts[i][1] * pts[i][1];
+            pq.add(new Point(pts[i][0],  pts[i][1], distSq, i));
+        }// nearest k cars
+        for (int i = 0; i < k; i++) {
+            System.out.println("C"+pq.remove().idx);
+        }
+    }
+}
