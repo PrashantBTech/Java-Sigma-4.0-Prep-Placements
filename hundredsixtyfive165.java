f005965305703e8a6701d6ddf7bb67259c4f0f90
PrashantBTech <dhimanprashant996@gmail.com>
2025-09-14 21:21:49 +0530

    Create hundredsixtyfive165.java

---
+++ hundredsixtyfive165.java
@@ -0,0 +1,54 @@
+// TRIES
+// 1. Insertion in O(L)
+// 2. Search in Trie in O(L)
+
+public class hundredsixtyfive165 {
+    static class Node {
+        Node children[] = new Node[26];
+        boolean eow = false;
+
+        Node() {
+            for (int i = 0; i < 26; i++) {
+                children[i] = null;
+            }
+        }
+
+    }
+
+    public static Node root = new Node();
+
+    public static void insert(String word) { // O(L) , L is length of largest word
+        Node curr = root;
+        for (int i = 0; i < word.length(); i++) {
+            int idx = word.charAt(i) - 'a';
+            if (curr.children[idx] == null) {
+                curr.children[idx] = new Node();
+            }
+            curr = curr.children[idx];
+        }
+
+        curr.eow = true;
+    }
+
+    public static boolean search(String key) { // O(L)
+        Node curr = root;
+        for (int level = 0; level < key.length(); level++) {
+            int idx = key.charAt(level) - 'a';
+            if (curr.children[idx] == null) {
+                return false;
+            }
+            curr = curr.children[idx];
+        }
+
+        return curr.eow == true;
+    }
+
+    public static void main(String[] args) {
+        String words[] = { "the", "a", "there", "their", "any", "thee" };
+        for (int i = 0; i < words.length; i++) {
+            insert(words[i]);
+        }
+        System.out.println(search("thee"));
+        System.out.println(search("thor"));
+    }
+}
