19d8d4e24a5e059f2c3f411f516af73261fab23a
PrashantBTech <dhimanprashant996@gmail.com>
2025-09-17 21:23:49 +0530

    Create hunderdsixtyeight168.java

---
+++ hunderdsixtyeight168.java
@@ -0,0 +1,58 @@
+
+// startsWithProblem
+
+public class hunderdsixtyeight168 {
+    static class Node {
+        Node children[] = new Node[26]; // 'a'-'z'
+        boolean eow = false;
+        int freq;
+
+        public Node() {
+            for (int i = 0; i < 26; i++) {
+                children[i] = null;
+            }
+            freq = 1;
+        }
+    }
+
+    public static Node root = new Node();
+
+    public static void insert(String word) {
+        Node curr = root;
+        for (int i = 0; i < word.length(); i++) {
+            int idx = word.charAt(i) - 'a';
+            if (curr.children[idx] == null) {
+                curr.children[idx] = new Node();
+            } else {
+                curr.children[idx].freq++;
+            }
+            curr = curr.children[idx];
+        }
+        curr.eow = true;
+    }
+
+    public static boolean startsWith(String prefix) { // O(l)
+        Node curr = root;
+
+        for (int i = 0; i < prefix.length(); i++) {
+            int idx = prefix.charAt(i) - 'a';
+            if (curr.children[idx] == null) {
+                return false;
+            }
+            curr = curr.children[idx];
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        String words[] = { "apple", "app", "mango", "man", "woman" };
+        String prefix1 = "app";
+        String prefix2 = "moon";
+
+        for (int i = 0; i < words.length; i++) {
+            insert(words[i]);
+        }
+        System.out.println(startsWith(prefix1));
+        System.out.println(startsWith(prefix2));
+    }
+}
